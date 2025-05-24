c97b49a6fa5d0c4d4365782000c198d03cccde80
Medha Bhargava <medha20072004@gmail.com>
2025-05-24 12:03:21 +0530

    Add files for Access Denied

---
+++ faculty-portfolio/src/components/AccessDenied/AccessDenied.jsx
@@ -0,0 +1,12 @@
+import "./AccessDenied.css"
+
+const AccessDenied = () => {
+    return (
+        <div className="text">
+            <h1 className="h1">🚫 Access Denied</h1>
+            <p className="para">You do not have permission to view this page.</p>
+        </div>
+    );
+};
+
+export default AccessDenied;
