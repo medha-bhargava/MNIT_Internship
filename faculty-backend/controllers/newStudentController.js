f196f2c4882123357fc5655d174740dec99b4813
Medha Bhargava <medha20072004@gmail.com>
2025-06-21 13:55:50 +0530

    Fix import

--- faculty-backend/controllers/newStudentController.js
+++ faculty-backend/controllers/newStudentController.js
@@ -1,5 +1,5 @@
 import NewStudent from '../models/newStudentModel.js';
-import User from '../models/userModel.js';
+import User from '../models/User.js';
 
 // Register New Student (pending approval)
 export const registerStudent = async (req, res) => {
